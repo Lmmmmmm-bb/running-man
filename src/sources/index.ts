938421da169226f1b86ab24d11e52933de771b54
_lmmmmmm <lmmmmmm12138@gmail.com>
2022-07-12 08:53:43 +0800

    feat: update running man 2012 episode

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,4 +1,5 @@
 import source2010 from './source-2010.json';
 import source2011 from './source-2011.json';
+import source2012 from './source-2012.json';
 
-export { source2010, source2011 };
+export { source2010, source2011, source2012 };
